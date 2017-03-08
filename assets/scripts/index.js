6805ca62d8ade6a8960ff4424703974a00c93a41
SquirtleSquad1988 <alexanderjayrobles0@gmail.com>
2017-03-08 10:51:04 -0500

    Create authorization functionality
    Added authorization functionality that enables the user to sign in,
    sign up, sign out, and change password. All of the functions are
    contained in a navbar at the top and only appear when their function
    is warranted. Also changed the api.origin url paths in
    assets/scripts/config.js and the project name in our package.json file.

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -2,10 +2,22 @@
 
 const setAPIOrigin = require('../../lib/set-api-origin');
 const config = require('./config');
+const handlers = require('./auth/events');
 
 $(() => {
   setAPIOrigin(location, config);
+  $('#change-password-dropdown').hide();
+  $('#exit').hide();
+  $('.create-new-album').hide();
+  $('.log').text('Sign In or Sign Up');
+  $('#exit').on('click', function () {
+    $('#sign-out').submit();
+  });
 });
+
+$(document).ready(handlers.addHandlers);
+
+
 
 // use require with a reference to bundle the file and use it in this file
 // const example = require('./example');
