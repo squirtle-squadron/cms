e373f9cf2550bf3d9f233e365963a37fc12d7ff2
ashlenprice <ashlen.price@gmail.com>
2017-03-09 18:49:18 -0500

    Implement  auto sign-in with sign-up
    Added the sign-in to sign-up in the auth/events.js so that when
    the sign-up event fires it automatically logs the user in.
    The function works, however in the chrome console there is a warning
    that says:
    
    'jQuery.Deferred exception: Cannot read property 'then' of undefined
    TypeError: Cannot read property 'then' of undefined
    at Object.<anonymous>'
    
    Not sure what this means, but will be filing an issue about this next.
    
    This should not affect anything else and will be making pull request
    to  merge into dev2.

--- assets/scripts/auth/events.js
+++ assets/scripts/auth/events.js
@@ -6,14 +6,6 @@ const api = require('./api');
 const ui = require('./ui');
 const store = require('../store');
 
-const onSignUp = function (event) {
-  event.preventDefault();
-  let data = getFormFields(event.target);
-  api.signUp(data)
-    .then(ui.signUpSuccess)
-    .catch(ui.signUpFailure);
-};
-
 const onSignIn = function (event) {
   event.preventDefault();
   let data = getFormFields(event.target);
@@ -25,6 +17,22 @@ const onSignIn = function (event) {
     .then(ui.signInSuccess)
     .catch(ui.signInFailure);
 };
+
+const onSignUp = function (event) {
+  event.preventDefault();
+  let data = getFormFields(event.target);
+  api.signUp(data)
+  .then(()=> {
+    onSignIn(event, data)
+    .then((response) => {
+          store.user = response.user;
+          return store.user;
+  })
+    .then(ui.signUpSuccess)
+    .catch(ui.signUpFailure);
+});
+};
+
 
 const onChangePassword = function (event) {
   event.preventDefault();
