19d6f90922f03983f734adfbf6ee05c903ea722c
SquirtleSquad1988 <alexanderjayrobles0@gmail.com>
2017-03-10 01:11:03 -0500

    Add handlebars files to dynamically load elements on page
    Three handlebars files and click handlers were added so upon page load
    the elements render.This will allow us each to work on our own separate
    files and should hopefully improve workflow among the group.

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -5,6 +5,15 @@ const config = require('./config');
 const pageEvents = require('./pages/events');
 const handlers = require('./auth/events');
 const blogEvents = require('./blog/events');
+const showNavbar = require('./templates/navbar.handlebars');
+const showPageStuff = require('./templates/page-crud.handlebars');
+const showBlogStuff = require('./templates/blog-crud.handlebars');
+
+$(document).ready(function () {
+  $('body').append(showNavbar);
+  $('.show-page').append(showPageStuff);
+  $('.show-blog').append(showBlogStuff);
+});
 
 $(() => {
   setAPIOrigin(location, config);
